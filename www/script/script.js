e4b717a6ac7310331ba78feaa18a7625cdd3c56e
James Mayell <james@nimbusdisability.com>
2026-07-16 11:13:02 +0100

    tweak: version

--- www/script/script.js
+++ www/script/script.js
@@ -218,7 +218,7 @@ window.copyRoomLink = () => {
 };
 
 window.endCall = async () => {
-    window.close();
+    window.location.href = 'https://concertdigital.co.uk';
 };
 
 async function toggleScreenShare() {
